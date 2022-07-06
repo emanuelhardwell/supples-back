bc6867b42ec7041dbd255fef727a768bb9b41cae
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-06 14:17:26 -0500

    [Schema] se crearon schemas para categorySchemas

---
+++ src/schemas/category.schema.js
@@ -0,0 +1,51 @@
+const Joi = require("joi");
+
+const categorySchema = {};
+
+categorySchema.createCategorySchema = Joi.object({
+  params: Joi.object({}),
+  query: Joi.object({}),
+  body: Joi.object({
+    name: Joi.string().min(3).max(25).required().messages({
+      "string.base": `Nombre debe ser un texto`,
+      "string.empty": `Nombre no puede venir vacio`,
+      "string.min": `Nombre debe tener minimo 3 letras`,
+      "string.max": `Nombre debe tener maximo 25 letras`,
+      "any.required": `Nombre es obligatorio`,
+    }),
+  }),
+});
+
+categorySchema.updateCategorySchema = Joi.object({
+  params: Joi.object({
+    id: Joi.number().required().messages({
+      "number.base": `Id debe ser un número`,
+      "number.empty": `Id no puede venir vacio`,
+      "any.required": `Id es obligatorio`,
+    }),
+  }),
+  query: Joi.object({}),
+  body: Joi.object({
+    name: Joi.string().min(3).max(25).required().messages({
+      "string.base": `Nombre debe ser un texto`,
+      "string.empty": `Nombre no puede venir vacio`,
+      "string.min": `Nombre debe tener minimo 3 letras`,
+      "string.max": `Nombre debe tener maximo 25 letras`,
+      "any.required": `Nombre es obligatorio`,
+    }),
+  }),
+});
+
+categorySchema.deleteCategorySchema = Joi.object({
+  params: Joi.object({
+    id: Joi.number().required().messages({
+      "number.base": `Id debe ser un número`,
+      "number.empty": `Id no puede venir vacio`,
+      "any.required": `Id es obligatorio`,
+    }),
+  }),
+  query: Joi.object({}),
+  body: Joi.object({}),
+});
+
+module.exports = categorySchema;
