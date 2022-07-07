8c4d1c6bff2096d4d19fd819e191268b293edecd
emanuelhardwell <emanuelnoe16@gmail.com>
2022-07-07 11:29:20 -0500

    [Model] se creo el modelo Product

---
+++ src/models/Product.model.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../db/db");
+
+const Product = sequelize.define(
+  "Product",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imageId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = Product;
